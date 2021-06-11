169bfaa03c22db8e96b6b5356828cda8e5991588
unknown <renatosmartins@msn.com>
2021-06-11 11:56:52 -0300

    Desafio 02

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,59 @@
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+import { Button } from '../components/Button';
+
+import '../styles/sidebar.scss';
+import '../styles/global.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
 export function SideBar() {
-  // Complete aqui
+    const [selectedGenreId, setSelectedGenreId] = useState(1);
+    const [movies, setMovies] = useState<MovieProps[]>([]);
+    const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+    const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+    useEffect(() => {
+        api.get<GenreResponseProps[]>('genres').then(response => {
+          setGenres(response.data);
+        });
+      }, []);
+
+    function handleClickButton(id: number) {
+      setSelectedGenreId(id);
+    }
+
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <nav className="sidebar">
+          <span>Watch<p>Me</p></span>
+          <div className="buttons-container">
+            {genres.map(genre => (
+              <Button
+                key={String(genre.id)}
+                title={genre.title}
+                iconName={genre.name}
+                onClick={() => handleClickButton(genre.id)}
+                selected={selectedGenreId === genre.id}
+              />
+            ))}
+          </div>
+        </nav>
+      </div>
+    )
 }
